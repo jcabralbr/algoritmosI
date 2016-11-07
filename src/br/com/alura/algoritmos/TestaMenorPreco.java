454679e90dcb42f49f0f6f341d480054ff7efe9c
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-11-07 12:11:25 -0200

    selection sort

--- src/br/com/alura/algoritmos/TestaMenorPreco.java
+++ src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -4,7 +4,6 @@ package br.com.alura.algoritmos;
 public class TestaMenorPreco {
 
     public static void main(String[] args){
-
         Produto[] produtos = {
             new Produto("Lamborghini", 1000000),
             new Produto("Jipe", 46000),
@@ -12,13 +11,6 @@ public class TestaMenorPreco {
             new Produto("Smart", 46000),
             new Produto("Fusca", 17000)
         };
-
-        double precos[] = new double[5];
-        precos[0] = 1000000;
-        precos[1] = 46000;
-        precos[2] = 16000;
-        precos[3] = 46000;
-        precos[4] = 17000;
 
         int maisBarato = buscaMenor(produtos, 0, produtos.length - 1);
 
@@ -28,7 +20,7 @@ public class TestaMenorPreco {
 
     private static int buscaMenor(Produto[] produtos , int inicio, int termino) {
         int maisBarato = inicio;
-        for (int atual = 0; atual <= termino; atual++){
+        for (int atual = inicio; atual <= termino; atual++){
             if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()){
                 maisBarato = atual;
             }
